4342e0d76ac3759ca64e01a0c9c529d9dbe33c13
Kyara Scott <ks914639@taz.cs.wcupa.edu>
2020-11-15 21:52:53 -0500

    updated indentation

--- Java/sum.java
+++ Java/sum.java
@@ -3,38 +3,37 @@ public class sum{
 
     public static final int  N = 1024;
     public static final int  M = 1024;
-   public static int i,j; public static int sum =0;
+    public static int i,j; public static int sum =0;
     public static int [][] array =  new int [M][N];
 	public sum(){
 
 	}
   public static void main (String[] args){
+   for(i=0; i<M; i++)
+      for(j=0; j<N; j++)
+	  array[i][j] = 1;
+   sum s = new sum();
+     s.firstSum();
+     s.secondSum();
+ }
 
-	for(i=0; i<M; i++)
-	   for(j=0; j<N; j++)
-		array[i][j] = 1;
- 	sum s = new sum();
-		s.firstSum();
-		s.secondSum();
-	}
-  
-   public void firstSum(){
+  public void firstSum(){
   final long startTime = System.nanoTime();
    for(i=0; i<M; i++)
       for(j=0; j<N; j++)
 	sum+= array[i][j];
   final long duration = System.nanoTime()-startTime;
-   System.out.println("It takes " + duration + " seconds and the sum is " + sum );
+  System.out.println("It takes " + duration + " seconds and the sum is " + sum );
   }
 
   public void secondSum(){
- sum = 0;
+  sum = 0;
   final long startTime = System.nanoTime();
    for(j=0; j<N; j++)
       for(i=0; i<M; i++)
 	sum+= array[j][i];
   final long duration = System.nanoTime()-startTime;
-   System.out.println("It takes " + duration + " seconds and the sums is " + sum );
+  System.out.println("It takes " + duration + " seconds and the sums is " + sum );
   }
 }
 
