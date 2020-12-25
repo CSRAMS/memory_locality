c9356dd0d3c36df4cbd10256188737233d044426
Kyara Scott <ks914639@taz.cs.wcupa.edu>
2020-12-24 20:44:35 -0500

    Updated go loops

--- go/go.go
+++ go/go.go
@@ -1,16 +1,39 @@
 package main
 
 import "fmt"
+import "time"
+var M = 1024
+var N = 1024
 
 func main(){
    var i int
    var k int
     sum := 0
-    var a[M][N]int
+      a := make([][]int, M)
+      for i := 0; i < M; i ++ {
+    	a[i] = make([]int, N)
+      	for j := 0; j < N; j++ {
+          a[i][j] = 1
+    }
+  }
 
-      for i := 0; i<M; i++{
-        for j := 0; j<N; j++{
-             a[i][j] = 1
-        }
-     }
+
+start := time.Now()
+ for i := 0; i < M; i++{
+    for j := 0; j < N; j++{
+       sum += a[i][j];
+   }
+ }
+duration := time.Since(start)
+fmt.Println(duration.Nanseconds())
+
+ sum2 := 0
+  startTime := time.Now()
+  for j := 0; j < N; j++{
+     for i := 0; i < M; i++{
+            sum2 += a[i][j]
+   }
+ }
+durationTime := time.Since(startTime)
+fmt.Println(durationTime.Nanoseconds())
 }
